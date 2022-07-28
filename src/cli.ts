f48e12876e7eec707a7d118d5fd0a76d834b69af
tarikgul <tarik@parity.io>
2022-07-28 16:32:58 -0400

    fix: replace argparse with yargs

--- src/cli.ts
+++ src/cli.ts
@@ -1,13 +1,31 @@
-import { ArgumentParser, Namespace } from 'argparse';
+import yargs from 'yargs';
 
-export const parseArgs = (): Namespace => {
-	const parser = new ArgumentParser();
-
-	parser.add_argument('-v', '--version', { action: 'store_true' });
-    // TODO add strict path check to make sure the path exists. 
-    parser.add_argument('-p', '--path');
-    parser.add_argument('-c', '--chain');
-    parser.add_argument('-a', '--availableChains', { action: 'store_true' })
-
-	return parser.parse_args() as Namespace;
-};
+export const argv = () => {
+    return yargs
+        .version(false)
+        .options({
+            'v': {
+                alias: 'version',
+                description: 'Version of the current package.',
+                type: 'number',
+                demandOption: false,
+            },
+            'p': {
+                alias: 'path',
+                description: 'Path to directory to generate Type files into.',
+                type: 'string',
+                demandOption: true,
+            },
+            'c': {
+                alias: 'chain',
+                description: 'A chain to generate types for. If this is not inputted it will create a types bundle for all chains.',
+                type: 'string',
+                demandOption: false,
+            },
+            'a': {
+                alias: 'availableChains',
+                description: 'List all available chains to generate types for.',
+                demandOption: false,
+            }
+        }).argv
+}
